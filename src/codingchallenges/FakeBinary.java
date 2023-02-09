00eff6615e5b4c08cd4611863a5274b00a7cf6a8
Edwin <Flattoedwin@gmail.com>
2023-02-08 23:28:54 -0500

    added comments to solutions

--- src/codingchallenges/FakeBinary.java
+++ src/codingchallenges/FakeBinary.java
@@ -1,7 +1,14 @@
 package codingchallenges;
 
 public class FakeBinary {
+	public static void main(String[] args) {
+		System.out.println(fakeBin("1234567"));
+	}
     public static String fakeBin(String numberString) {
+    	 // Replace all digits below 5 with '0'
+        // Replace all digits 5 and above with '1'
+    	
+    	//used replaceAll to replace all occorrences of the specified regex with either "0" or "1"
         return numberString.replaceAll("[0-4]", "0").replaceAll("[5-9]", "1");
     }
 }
