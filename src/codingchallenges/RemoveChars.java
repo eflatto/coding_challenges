700a9f40890802dcb043ab63efc1bcda92fccc45
Edwin <Flattoedwin@gmail.com>
2023-02-09 21:57:29 -0500

    added new solution

---
+++ src/codingchallenges/RemoveChars.java
@@ -0,0 +1,22 @@
+package codingchallenges;
+/*
+ * Question
+ * It's pretty straightforward. Your goal is to create a 
+ * function that removes the first and last characters of a string. 
+ * You're given one parameter, the original string. You 
+ * don't have to worry with strings with less than two characters.
+ */
+public class RemoveChars {
+	public static void main(String[] args) {
+		String hello="hello";
+		System.out.println(remove(hello));
+	}
+
+	public static String remove(String str) {
+
+		str = str.substring(1, str.length() - 1);
+
+		return str;
+	}
+
+}
