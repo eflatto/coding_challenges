50f5568a60f7923dd41653619c208f53a5e7b492
Edwin <Flattoedwin@gmail.com>
2023-02-09 22:56:11 -0500

    new solution added

--- src/codingchallenges/ShortestWord.java
+++ src/codingchallenges/ShortestWord.java
@@ -1,4 +1,10 @@
 package codingchallenges;
+
+/*Simple, given a string of words, return the length of the 
+ * shortest word(s).
+String will never be empty and you do not need to account for different data types.
+ */
+
 public class ShortestWord {
 	public static void main(String[] args) {
 		System.out.println(findShort("bitcoin take over the world maybe who knows perhaps"));
